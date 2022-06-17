96d1a240bd76dae78260a9ed14cbce0cbe80f827
hyejuc <107419210+hyejuc@users.noreply.github.com>
2022-06-18 03:18:22 +0900

    [Silver III] Title: N과 M (3), Time: 2052 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+import sys
+
+N,M=map(int,sys.stdin.readline().split())
+temp=[]
+def dfs():
+    if len(temp)==M:
+        print(*temp)
+        return
+    for i in range(1,N+1):
+        temp.append(i)
+        dfs()
+        temp.pop()
+dfs()
